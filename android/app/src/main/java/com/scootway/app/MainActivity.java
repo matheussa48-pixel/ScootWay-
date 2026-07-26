219ed12f97a8a5f38520d6f02c6f9cc061b0d268
matheussa48-pixel <matheussa48@gmail.com>
2026-07-26 12:36:19 -0300

    add workflow for build

---
+++ android/app/src/main/java/com/scootway/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.scootway.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scootway.app',
+  appName: 'ScootWay',
+  webDir: 'dist'
+};
+
+export default config;
